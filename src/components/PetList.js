0d9a7640dee00275aac258b929c6ba09b3b15f0a
BarbaraWidjono <barbarawidjono@gmail.com>
2018-12-10 15:17:12 -0800

    details callback for PetList

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -6,13 +6,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const PetList = (props) => {
+  let pets = props.petInfo;
 
+  const details = (id) => {
+    console.log("Inside the PetList");
+    console.log(id);
+  }
+
+  const petCollection = pets.map((pet, i) => {
+    return <PetCard
+      key={i}
+      id={pet.id}
+      name={pet.name}
+      species={pet.species}
+      about={pet.about}
+      location={pet.location}
+      detailsCallback = {details}/>
+  })
 
   return (
     <div className="card-group">
+      <li>
+        {petCollection}
+      </li>
     </div>
   )
 }
+
 
 PetList.propTypes = {
   pets: PropTypes.array.isRequired,
